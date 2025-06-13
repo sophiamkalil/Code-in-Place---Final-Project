57e531d687ea5bf69e01036382ffe2534690f636
sophiamkalil <sophia.mkalil@gmail.com>
2025-06-12 22:15:55 -0300

    store_die

--- functions.py
+++ functions.py
@@ -8,3 +8,18 @@ def roll_dice(num_dice):
         dice_values.append(die_value)
     
     return dice_values
+
+def store_die(rolled_dice, stored_dice, index):  # index: index of the rolled dice list to be moved to stored dice
+    new_rolled_dice = []
+    result = []
+    stored_die = rolled_dice[index]
+    stored_dice.append(stored_die)
+
+    for i in range(len(rolled_dice)):
+        if i != index:
+            new_rolled_dice.append(rolled_dice[i])
+
+    result.append(new_rolled_dice)
+    result.append(stored_dice)
+
+    return result
